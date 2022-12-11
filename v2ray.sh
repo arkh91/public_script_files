214ca684f275ae774cd5ca3482c47dec0a3a701c
Ali <39136692+arkh91@users.noreply.github.com>
2022-12-10 19:28:59 -0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update && apt upgrade -y 
+#https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 
 echo -e "\033[32mSystem updated.\033[m"
 
@@ -10,9 +11,11 @@ echo -e "\033[35mlibssl1.1_1.1.1f-1ubuntu2_amd64.deb is downloaded.\033[m"
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 echo -e "\033[32mlibssl1.1_1.1.1f-1ubuntu2_amd64.deb is installed.\033[m"
 
+read -p "Press enter to continue"
+
 sudo wget https://github.com/korn-sudo/Project-Fog/raw/main/files/installer/ubv301b && chmod +x ./ubv301b && ./ubv301b -y
 
+echo -e "\033[36mInstallation is finished.\033[m"
 
-
+#https://www.youtube.com/watch?v=BGIHEAd35do
 #sudo wget https://github.com/arkh91/public_script_files/blob/main/v2ray.sh && chmod +x ./v2ray.sh -y
-

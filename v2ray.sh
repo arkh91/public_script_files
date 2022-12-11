3689535287de8f93e04a59dc4de63918e0bf60ba
Ali <39136692+arkh91@users.noreply.github.com>
2022-12-10 18:45:52 -0800

    Update v2ray.sh
    updated

--- v2ray.sh
+++ v2ray.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update && apt upgrade -y 
+sudo apt-get install libssl1.1  
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+
+sudo wget https://github.com/korn-sudo/Project-Fog/raw/main/files/installer/ubv301b && chmod +x ./ubv301b && ./ubv301b

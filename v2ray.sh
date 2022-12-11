709ec8d14c5d28db5cd653e1d76cc3d5cc350ac8
Ali <39136692+arkh91@users.noreply.github.com>
2022-12-10 19:20:32 -0800

    colorfull comments added

--- v2ray.sh
+++ v2ray.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 sudo apt-get update && apt upgrade -y 
+
+echo -e "\033[32mSystem updated.\033[m"
+
 sudo apt-get install libssl1.1  
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+echo -e "\033[35mlibssl1.1_1.1.1f-1ubuntu2_amd64.deb is downloaded.\033[m"
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+echo -e "\033[32mlibssl1.1_1.1.1f-1ubuntu2_amd64.deb is installed.\033[m"
+
+sudo wget https://github.com/korn-sudo/Project-Fog/raw/main/files/installer/ubv301b && chmod +x ./ubv301b && ./ubv301b -y
 
 
-sudo wget https://github.com/korn-sudo/Project-Fog/raw/main/files/installer/ubv301b && chmod +x ./ubv301b && ./ubv301b
+
+#sudo wget https://github.com/arkh91/public_script_files/blob/main/v2ray.sh && chmod +x ./v2ray.sh -y
+

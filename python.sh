02eabe0212dff0c06ba2493e757cc5f41f2292a9
Ali <39136692+arkh91@users.noreply.github.com>
2022-12-13 13:07:15 -0800

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#https://docs.python-guide.org/starting/install3/linux/
+sudo apt update
+echo -e "\033[32mSystem updated.\033[m"
+
+sudo apt install software-properties-common
+echo -e "\033[32msoftware-properties-common installed.\033[m"
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt-get install python3.8
+echo -e "\033[32mpython3.8.\033[m"
+
+echo -e "\033[36mPython is ready!\033[m"
+
+python3 --version
+read -p "Press enter to continue"
+
+#sudo wget https://github.com/arkh91/public_script_files/blob/main/python.sh && chmod +x ./python.sh

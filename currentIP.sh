a9b39b5ed132cf08a1334579c61b575d05c853a5
Ali <39136692+arkh91@users.noreply.github.com>
2023-03-02 15:07:39 -0800

    Update currentIP.sh

--- currentIP.sh
+++ currentIP.sh
@@ -5,3 +5,4 @@ extaddr=$(curl -s ifconfig.me/ip)
 echo "The external IP address is $extaddr (from cURL)"
 
 #chmod +x currentIP.sh
+#sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/currentIP.sh 

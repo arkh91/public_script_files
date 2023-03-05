f9f78d0e5cd163cb824164ca5cbb0a24d4a531fd
Ali <39136692+arkh91@users.noreply.github.com>
2023-03-04 20:17:42 -0800

    Update auto_download.sh

--- auto_download.sh
+++ auto_download.sh
@@ -32,6 +32,7 @@ TWENTYTHREE="23"
 
 download () {
 		wget https://dl3.soft98.ir/win/Microsoft.Windows.10.Pro-22H2.19045.2673.x64.part1.rar?1677800518
+		rm Microsoft.Windows.10.Pro-22H2.19045.2673.x64.part1.rar?1677800518
 }
 
 compare () {
@@ -137,39 +138,4 @@ compare () {
 }
 
 compare
-
-
-
-
-: '
-show(){
-        local x=$1
-        local y=$2
-        local txt="$3"
-        # Set cursor position on screen
-        tput cup $x $y
-        echo "$txt"
-}
-while [ : ]
-do
-        clear
-        # Get the system time
-        now="$(date +"%r")"
-        # Show main - menu, server name and time
-        show 5 10 "MAIN MENU for $HOSTNAME - $now"
-        show 6 10 "1. System info"
-        show 7 10 "2. Apache server stats"
-        show 8 10 "3. MySQL server stats"
-        show 9 10 "4. Firewall stats"
-        show 10 10 "5. Exit"
-        tput cup 11 10; read -t 2 -p "Choice [1-5] ? " usrch
-        # do something
-        case $usrch in
-                1) read -t 2 -p "Showing system info, wait..." fakeinput;;
-                2) read -t 2 -p "Showing apache info, wait..." fakeinput;;
-                3) read -t 2 -p "Showing mysqld info, wait..." fakeinput;;
-                4) read -t 2 -p "Showing firewall info, wait..." fakeinput;;
-                5) echo "Bye."; exit 0;;
-        esac
-done
-'
+#sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/auto_download.sh && chmod +x auto_download.sh

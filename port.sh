b9d9839cf559265efc7e2f7ae14fc7a02e58b44d
Ali <39136692+arkh91@users.noreply.github.com>
2024-01-04 15:24:20 -0800

    Lastet version

--- port.sh
+++ port.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-
 # Function to display usage information
 function show_usage {
     echo "Usage: $0 [-h | -help] <port_number>"
 }
-
 # Check if any arguments are provided
 if [ $# -eq 0 ]; then
     show_usage
     exit 1
 fi
-
 # Check for help options
 if [ "$1" == "-h" ] || [ "$1" == "-help" ]; then
     show_usage
     exit 0
 fi
-
 # Extract the port number from the command-line argument
 port_number=$1
-
-# Run the netstat command and use awk to filter unique IP addresses
-netstat -tn | grep ":$port_number.*ESTABLISHED" | awk '!seen[$5]++'
+# Run the netstat command
+netstat -tn | grep ":$port_number.*ESTABLISHED"
 
 
 #sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/port.sh && chmod u+x port.sh
+#This works fine

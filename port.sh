48170fad2b3c4c92781ff18028ee1e0658b69406
Ali <39136692+arkh91@users.noreply.github.com>
2024-01-04 15:26:45 -0800

    sample added

--- port.sh
+++ port.sh
@@ -17,7 +17,7 @@ fi
 port_number=$1
 # Run the netstat command
 netstat -tn | grep ":$port_number.*ESTABLISHED"
-
+#Sample: netstat -tn | grep ':18687.*ESTABLISHED'
 
 #sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/port.sh && chmod u+x port.sh
 #This works fine

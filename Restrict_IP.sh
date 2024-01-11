cc0ae7a8eee2afc671bc284933c8edfd249c9f8b
Ali <39136692+arkh91@users.noreply.github.com>
2024-01-10 18:09:46 -0800

    Last line

--- Restrict_IP.sh
+++ Restrict_IP.sh
@@ -20,6 +20,7 @@ done < "$filename"
 
 #Reads a file and executes all lines that do not start with a # character in the file.
 #./Restrict_IP.sh your_file.txt
-#sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/Restrict_IP.sh && chmod +x Restrict_IP.sh
 #sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/iran-firewall.txt
+#sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/Restrict_IP.sh && chmod +x Restrict_IP.sh && ./Restrict_IP.sh iran-firewall.txt
+
 

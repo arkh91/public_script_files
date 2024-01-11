5908a3fd06d1ddd47c00017f1f5d37c67b02e59a
Ali <39136692+arkh91@users.noreply.github.com>
2024-01-10 16:17:44 -0800

    First commit

---
+++ Restrict_IP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+filename=$1
+
+if [ ! -f "$filename" ]; then
+    echo "Error: File $filename not found."
+    exit 1
+fi
+
+while IFS= read -r line; do
+    if [[ ! "$line" =~ ^\# ]]; then
+        eval "$line"
+    fi
+done < "$filename"
+
+#Reads a file and executes all lines that do not start with a # character in the file.
+#./Restrict_IP.sh your_file.txt
+#sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/Restrict_IP.sh && chmod +x Restrict_IP.sh
+

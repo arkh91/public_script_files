c30872f252e31a3869fb462798d3604fe0a208f8
Ali <39136692+arkh91@users.noreply.github.com>
2024-01-12 19:47:39 -0800

    Accept loop commented out.

--- Restrict_IP_range.sh
+++ Restrict_IP_range.sh
@@ -34,38 +34,16 @@ while IFS=$'\t' read -r start_ip end_ip; do
 done < "$file_path"
 
 # Iterate through each line in the file
-while IFS= read -r line; do
+#while IFS= read -r line; do
     # Check if the line starts with $
-    if [[ $line == \$* ]]; then
+    #if [[ $line == \$* ]]; then
         # Extract the IP address from the line (assuming it's in the format $IP_ADDRESS)
-        ip_address="${line#\$}"
+        #ip_address="${line#\$}"
 
         # Accept the connection using the extracted IP address
-        echo "Accepting connection from IP: $ip_address"
-        sudo iptables -A INPUT -s $ip_address -j ACCEPT
-    fi
-done < "$file_path"
-
-# Check if the file exists
-#if [ ! -f "$file_path" ]; then
-#    echo "File not found: $file_path"
- #   exit 1
-#fi
-
-# Read each line from the file
-#while IFS= read -r line; do
-    # Ignore lines starting with '#' (comments)
- #   if [[ "$line" =~ ^[^#] ]]; then
-        # Extract the IP address from the line
-#        ip_address=$(echo "$line" | awk '{print $1}')
-        
-        # Perform your desired action with the IP address
-        # For example, you can use iptables to accept the IP
-#        iptables -A INPUT -s "$ip_address" -j ACCEPT
-        
-        # Print a message indicating the action
-#        echo "Accepted IP: $ip_address"
-#    fi
+        #echo "Accepting connection from IP: $ip_address"
+        #sudo iptables -A INPUT -s $ip_address -j ACCEPT
+    #fi
 #done < "$file_path"
 
 #sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/Restrict_IP_range.sh && chmod +x Restrict_IP_range.sh 

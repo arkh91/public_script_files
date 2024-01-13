886aff7fa67e7b1dcb1addfe36b92a3ae31803b6
Ali <39136692+arkh91@users.noreply.github.com>
2024-01-12 16:55:18 -0800

    testing1

--- Restrict_IP_range.sh
+++ Restrict_IP_range.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
+
+# Check if the file exists
+if [ -f "path.txt" ]; then
+    # Read the content of path.txt and use it as an argument
+    path_argument=$(<path.txt)
+    
+    # Your code that uses the path_argument goes here
+    echo "Using path argument: $path_argument"
+    
+    # Rest of your script...
+else
+    echo "Error: path.txt does not exist."
+    exit 1
+fi
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <path_to_file>"
     exit 1
 fi
 
-file_path=$1
+#file_path=$1
 
 # Check if the file exists
-if [ ! -f "$file_path" ]; then
-    echo "File not found: $file_path"
-    exit 1
-fi
+#if [ ! -f "$file_path" ]; then
+#    echo "File not found: $file_path"
+ #   exit 1
+#fi
 
 # Read each line from the file
-while IFS= read -r line; do
+#while IFS= read -r line; do
     # Ignore lines starting with '#' (comments)
-    if [[ "$line" =~ ^[^#] ]]; then
+ #   if [[ "$line" =~ ^[^#] ]]; then
         # Extract the IP address from the line
-        ip_address=$(echo "$line" | awk '{print $1}')
+#        ip_address=$(echo "$line" | awk '{print $1}')
         
         # Perform your desired action with the IP address
         # For example, you can use iptables to accept the IP
-        iptables -A INPUT -s "$ip_address" -j ACCEPT
+#        iptables -A INPUT -s "$ip_address" -j ACCEPT
         
         # Print a message indicating the action
-        echo "Accepted IP: $ip_address"
-    fi
-done < "$file_path"
+#        echo "Accepted IP: $ip_address"
+#    fi
+#done < "$file_path"
 
 #sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/Restrict_IP_range.sh && chmod +x Restrict_IP_range.sh 

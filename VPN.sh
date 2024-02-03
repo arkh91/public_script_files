46c25a92f25b95b4cdfc461f52bdc582792154f7
Ali <39136692+arkh91@users.noreply.github.com>
2024-02-03 13:06:48 -0800

    Update VPN.sh

--- VPN.sh
+++ VPN.sh
@@ -8,7 +8,7 @@
 #######################################
 
 if [ "$EUID" -ne 0 ]
-  then echo -e "\033[31mPlease run as root\033[m" 
+  then echo -e "\033[31mPlease run as root\033[m"
   exit
 else
   echo -e "\033[32mAdding iran-firewall-range2\033[m"

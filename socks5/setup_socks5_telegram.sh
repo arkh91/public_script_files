a70c8dc94a35f5998e5115dccfc97e91490f3a93
Ali <39136692+arkh91@users.noreply.github.com>
2024-10-20 02:04:00 -0700

    Update setup_socks5_telegram.sh

--- socks5/setup_socks5_telegram.sh
+++ socks5/setup_socks5_telegram.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# 1. Install Shadowsocks if not already installed
-sudo apt update
-sudo apt install -y shadowsocks-libev jq
+# 1. Check if Shadowsocks and jq are installed, then skip the installation if found
+if ! command -v ss-server &> /dev/null || ! command -v jq &> /dev/null
+then
+    echo "Shadowsocks or jq is not installed. Installing now..."
+    sudo apt update
+    sudo apt install -y shadowsocks-libev jq
+else
+    echo "Shadowsocks and jq are already installed. Skipping installation."
+fi
 
 # 2. Get the server's public IP address
 PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
@@ -42,5 +48,6 @@ echo "Server IP: $PUBLIC_IP"
 echo "Port: $SERVER_PORT"
 echo "Password: $PASSWORD"
 echo "Unique User Telegram Proxy Link: $USER_LINK"
+
 
 #wget https://raw.githubusercontent.com/arkh91/public_script_files/refs/heads/main/socks5/setup_socks5_telegram.sh && chmod +x setup_socks5_telegram.sh

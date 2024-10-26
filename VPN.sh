c04acbf22ab1858ccb180e6df3741691cdefeb33
Ali <39136692+arkh91@users.noreply.github.com>
2024-10-26 14:43:48 -0700

    Update VPN.sh

--- VPN.sh
+++ VPN.sh
@@ -97,8 +97,8 @@ else
   else
     echo "x-ui MHSanaei installation not found."
     #bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-    bash <(curl -Ls https://raw.githubusercontent.com/NidukaAkalanka/x-ui-english/master/install.sh)
-    docker restart shadowbox
+    #bash <(curl -Ls https://raw.githubusercontent.com/NidukaAkalanka/x-ui-english/master/install.sh)
+    #docker restart shadowbox
   fi
   
   ls

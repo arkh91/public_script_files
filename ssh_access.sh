e32c01d2448a3d5a7141936b05797e7577d990dd
Ali <39136692+arkh91@users.noreply.github.com>
2024-11-08 21:14:35 -0800

    Create ssh_access.sh

---
+++ ssh_access.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#######################################
+#                                     #
+#  This code has been written by:     #
+#  https://github.com/arkh91/         #                        
+#                                     #
+#######################################
+
+# Generate SSH key pair
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
+
+# Display fingerprint of the public key
+ssh-keygen -lf ~/.ssh/id_rsa.pub
+
+# Restart SSH service
+sudo service ssh restart
+
+echo -e "\e[32mSSH key generated, fingerprint displayed, and SSH service restarted.\e[0m"
+

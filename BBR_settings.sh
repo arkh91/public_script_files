824680705ea21d88c337d07c4f87ffb72fc7567b
Ali <39136692+arkh91@users.noreply.github.com>
2024-11-10 17:00:57 -0800

    enable_bbr

--- BBR_settings.sh
+++ BBR_settings.sh
@@ -1,62 +1,31 @@
 #!/bin/bash
 
-# Function to check if BBR is already installed
-check_bbr_installed() {
-    if grep -qw "bbr" /proc/sys/net/ipv4/tcp_available_congestion_control; then
-        echo "BBR is already installed."
-        return 0
-    else
-        echo "BBR is not installed."
-        return 1
-    fi
-}
-
-# Install BBR if not already installed
-install_bbr() {
-    if check_bbr_installed; then
-        echo "BBR is already installed. Skipping installation."
-    else
-        echo "Installing BBR..."
-        
-        # Check the kernel version
-        kernel_version=$(uname -r | awk -F '.' '{print $1 "." $2}')
-        if (( $(echo "$kernel_version < 4.9" | bc -l) )); then
-            echo "Kernel version is below 4.9. Please upgrade your kernel to use BBR."
-            exit 1
-        fi
-
-        # Load BBR module
-        modprobe tcp_bbr
-
-        # Persist the module for future reboots
-        echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/bbr.conf
-        echo
-        echo "BBR has been installed, but it is not enabled."
-    fi
-}
-
-
 # Enable BBR
 #https://wiki.crowncloud.net/?How_to_enable_BBR_on_Ubuntu_20_04
 enable_bbr() {
-    # If BBR is not installed, install and enable it
-    if ! check_bbr_installed; then
-        echo -e "\e[91mBBR not detected. Please install it.\e[0m"
-    else
-        echo "Enabling BBR..."
-        echo -e "\nnet.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+# Check the kernel version
+    kernel_version=$(uname -r | awk -F '.' '{print $1 "." $2}')
+    if (( $(echo "$kernel_version < 4.9" | bc -l) )); then
+        echo -e "\e[91mKernel version is below 4.9. Please upgrade your kernel to use BBR.\e[0m"
+        exit 1        
+    elif ! grep -q "net.core.default_qdisc=fq" /etc/sysctl.conf || ! grep -q "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf; then
+        echo "Enabling BBR."
+        echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+        echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
         sudo sysctl -p
+        #echo -e "\nnet.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
 
         # Check the current TCP congestion control setting
-        current_congestion_control=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
+        #current_congestion_control=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
         #current_congestion_control=$(sysctl -n net.ipv4.tcp_congestion_control)
-        
-        # Verify if it's set to "bbr"
-        if [ "$current_congestion_control" = "bbr" ]; then
-            echo "BBR is enabled."
-        else
-            echo -e "\e[91mBBR is not enabled. Please try again later.\e[0m"
-        fi
+    fi
+    # Verify if it's set to "bbr"
+    # Check the current TCP congestion control setting
+    current_congestion_control=$(sysctl -n net.ipv4.tcp_congestion_control)
+    if [ "$current_congestion_control" = "bbr" ]; then
+        echo "BBR is enabled."
+    else
+        echo -e "\e[91mBBR is not enabled. Please try again later.\e[0m"
     fi
 }
 
@@ -80,10 +49,9 @@ bbr_menu (){
         echo "***********************************************************"
         echo "** BBR Installation and Management Script                **"
         echo "**                                                       **"
-        echo "** 1) Install BBR                                        **"
-        echo "** 2) Enable BBR                                         **"  
-        echo "** 3) Disable BBR                                        **"
-        echo "** 4) Back                                               **"
+        echo "** 1) Enable BBR                                         **"  
+        echo "** 2) Disable BBR                                        **"
+        echo "** 3) Back                                               **"
         echo "**                                                       **"
         echo "***********************************************************"
         echo "***********************************************************"
@@ -93,18 +61,14 @@ bbr_menu (){
         
         case $bbr_option in
             1)
-                install_bbr
-                read -p "Press enter to continue"
-                ;;
-            2)
                 enable_bbr
                 read -p "Press enter to continue"
                 ;;
-            3)
+            2)
                 disable_bbr
                 read -p "Press enter to continue"
                 ;;
-            4)
+            3)
                 bash <(curl -Ls https://bit.ly/arkh91_VPN)
                 exit 0
                 ;;
